8da692a3ea94d49019ed4ff85cb404f297af2afd
ChavichalovEvgeniy <evgeny.chavychalov.ex.et@gmail.com>
2021-07-01 15:57:03 +0300

    Fix typo

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import Link from "next/Link";
+import Link from "next/link";
 import { Link as MaterialLink, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
